da6d1718c2681ab4a5e8a602b195aa48676a831f
mohanishvenkatesh <vmohanish2000@hotmail.com>
2023-05-06 09:50:54 +0530

    project initialized.

---
+++ src/test/java/com/mohanish/personalaccounts/PersonalAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohanish.personalaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
